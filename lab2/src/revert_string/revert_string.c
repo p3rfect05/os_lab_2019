e11149c13c32badc82aa89aecab8ba443a6f3f84
Военушкин Александр <voenushkin_aa@magnit.ru>
2024-09-16 23:03:02 +0300

    lab2

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,15 @@
+#include <string.h>
 #include "revert_string.h"
 
 void RevertString(char *str)
 {
-	// your code here
+	int length = strlen(str);
+	int i;
+
+	for (i = 0; i < length / 2; i++){
+		char temp = str[i];
+		str[i] = str[length - i - 1];
+		str[length - i - 1] = temp;
+	}
 }
 
